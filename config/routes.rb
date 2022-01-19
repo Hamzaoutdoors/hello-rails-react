7416c5dcbec766b97e16b9c3721bcb8839b86b7f
Hamzaoutdoors <ellaouzihamza@gmail.com>
2022-01-19 01:44:45 +0100

    feat: Add routes for all components

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
-    get 'things', to: 'things#index'
+    get 'greetings', to: 'greetings#index'
   end
 
   get '*page', to: 'static#index', constraints: ->(req) do
