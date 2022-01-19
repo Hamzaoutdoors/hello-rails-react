d08e40f2b853efb5bf03211a9e0f633e59a58ee0
Hamzaoutdoors <ellaouzihamza@gmail.com>
2022-01-19 01:45:40 +0100

    feat: Add more two page for this hello project

--- app/javascript/pages/Home.js
+++ app/javascript/pages/Home.js
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <>
+    <div className="container mt-5">
       <h1>Welcome to the greetings page!</h1>
       <p>Please, click on the link below to be properly greeted.</p>
-      <Link to="/greetings">Click here!</Link>
-    </>
+      <Link to="/greetings" className="btn btn-warning mx-3">Click here!</Link>
+      <Link to="/v1/greetings" className="btn btn-info mx-3">More detail</Link>
+    </div>
   );
 }
 
