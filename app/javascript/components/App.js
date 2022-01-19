8c6a2883b14ab2504fbda32dc4bac568cfc4a871
Hamzaoutdoors <ellaouzihamza@gmail.com>
2022-01-19 01:45:06 +0100

    feat: Add App components that render all pages

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import PropTypes from "prop-types"
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
 
 import Home from '../pages/Home'
 import Greeting from '../pages/Greeting'
 
 const App = () => {
     return (
-        <Provider>
-            <BrowserRouter>
+        <Provider store={store}>
+            <Router>
                 <Routes>
                     <Route exact path="/" element={<Home />}/>
-                    <Route exact path="/greetings" element={<Greeting />}/>
+                    <Route path="/greetings" element={<Greeting />} />
                 </Routes>
-            </BrowserRouter>    
+            </Router>    
         </Provider>
     );
 }
-
-export default App
+export default App;
 
